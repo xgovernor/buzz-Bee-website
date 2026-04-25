ad845ec4779575ff67b81285ca2fbf99e838f5b0
xgovernor <abut1081@gmail.com>
2026-04-25 06:09:27 +0600

    feat: implement project showcase slider using keen-slider and add global navigation and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,24 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.buzzandbee.com",
+  trailingSlash: "always",
+  compressHTML: true,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
+  build: {
+    inlineStylesheets: 'always'
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: true,
+    },
   },
-  // Add your domain name here
-  site: "https://yourwebsite.com/",
-  integrations: [ sitemap()]
+  integrations: [sitemap({
+    trailingSlash: true
+  })]
 });
